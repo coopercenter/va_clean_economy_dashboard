50f10a999f10fc0237250245418376f70da51dd8
christinazhongg <60795006+christinazhongg@users.noreply.github.com>
2020-05-21 10:28:17 -0400

    updating the credentials

--- my_postgres_credentials.R
+++ my_postgres_credentials.R
@@ -1,5 +1,3 @@
-dbname   <- "postgres"
-db_host     <- "eo43.postgres.database.azure.com"
-port     <- 5432
-db_user     <- 'ccps-ceps@eo43'
-ra_pwd <- "apps88:Splash"
+db_host <- "va-energy2.postgres.database.azure.com"
+db_user <- "my username"
+ra_pwd<-  "my password"
