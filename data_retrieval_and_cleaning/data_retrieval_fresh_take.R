f741746eb178199725c4b809dd01b51c64745637
Emily Lien <egl6a@virginia.edu>
2022-04-21 10:09:56 -0400

    updates to the new data retreival process

--- data_retrieval_and_cleaning/data_retrieval_fresh_take.R
+++ data_retrieval_and_cleaning/data_retrieval_fresh_take.R
@@ -1,4 +1,10 @@
-#Load the packages and connect to the database, run the 'source' code for the data you want to update, then disconnect from the database
+"This file acts as a top-level resource for all the data retrieval processes used for the dashboard, and a directory for
+what each source provides to the plots.
+See the sourced files for details on how each data source is retrieved and stored, and for troubleshooting source-specific 
+errors
+Load the packages and connect to the database, run the 'source' code for the data you want to update, then disconnect 
+from the database"
+
 lbry<-c("data.table", "RPostgreSQL",  "tidyr", "dplyr","arrow","stringr",
         "tools","lubridate", "Hmisc", "here", "readxl","read_xlsx",'httr','jsonlite')
 test <- suppressMessages(lapply(lbry, require, character.only=TRUE, warn.conflicts = FALSE, quietly = TRUE))
@@ -8,21 +14,40 @@ db_driver = dbDriver("PostgreSQL")
 source(here::here("my_postgres_credentials.R"))
 db <- dbConnect(db_driver,user=db_user, password=ra_pwd,dbname="postgres", host=db_host)
 
-source(here("data_retrieval_and_cleaning/fetch_from_eia_api.R")) #updates eia_annual_data, most heavily used in the dashboard
+#EIA DATA------------------------------------------------------------------------------------------------------------------
+#updates eia_annual_data, most heavily used in the dashboard
+source(here("data_retrieval_and_cleaning/fetch_from_eia_api.R")) 
 
-source(here('data_retrieval_and_cleaning/FRED_series_retrieval.R')) #population data
+#fetch the plant capacity data
+source(here('data_retrieval_and_cleaning/fetch_eia860_data.R'))
 
-source(here('data_retrieval_and_cleaning/calculate_intensity_data.R')) #requires the EIA and FRED data to calculate updates
+#FRED DATA-----------------------------------------------------------------------------------------------------------------
+#population data
+source(here('data_retrieval_and_cleaning/FRED_series_retrieval.R')) 
 
+#INTENSITY DATA------------------------------------------------------------------------------------------------------------
+#requires the EIA and FRED data to calculate updates, provides energy use per capita and per GDP
+source(here('data_retrieval_and_cleaning/calculate_intensity_data.R'))
+
+#VCEA DATA-----------------------------------------------------------------------------------------------------------------
+#use for charting the renewable portfolio schedule
 source(here('data_retrieval_and_cleaning/reading_in_VCEA_provisions.R')) 
 
-source(here('data_retrieval_and_cleaning/fetch_eia860_data.R')) #plant capacity data
+#ENERGYCAP DATA------------------------------------------------------------------------------------------------------------
+#used for the energy efficiency page, tracking state building energy use over time
+source(here('data_retrieval_and_cleaning/retrieving_energycap_data.R')) 
 
-source(here('data_retrieval_and_cleaning/retrieving_SEDS_emissions_by_fuel.R')) #used for emissions by fuel for one graph
+#ENERGY EFFICIENCY MANDATE DATA--------------------------------------------------------------------------------------------
+#used for the energy efficiency page, tracking progress on a number of state mandates
+source(here('data_retrieval_and_cleaning/read_ve_mandate_sheets.R')) 
 
-source(here('data_retrieval_and_cleaning/retrieving_energycap_data.R')) #used for the energy efficiency page, tracking state building energy use over time
+#OFFSHORE WIND DATA--------------------------------------------------------------------------------------------------------
+#used for the offshore wind capacity values
+source(here('data_retrieval_and_cleaning/offshore_wind_capacity_retrieval.R'))
 
-source(here('data_retrieval_and_cleaning/read_ve_mandate_sheets.R')) #used for the energy efficiency page, tracking progress on a number of state mandates
+#SEDS EMISSIONS DATA-------------------------------------------------------------------------------------------------------
+#emissions data
+source(here('data_retrieval_and_cleaning/fetch_seds_emissions_by_fuel.R'))
 
 dbDisconnect(db)
 
