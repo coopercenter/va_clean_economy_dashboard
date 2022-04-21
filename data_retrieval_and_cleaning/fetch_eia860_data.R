dba6219e5ea110711a74dfe18770c795ee2015b2
Emily Lien <egl6a@virginia.edu>
2022-04-21 10:12:31 -0400

    Revamping this process to produce less clutter and perform actions in slightly fewer steps

--- data_retrieval_and_cleaning/fetch_eia860_data.R
+++ data_retrieval_and_cleaning/fetch_eia860_data.R
@@ -1,35 +1,33 @@
 #Write a function to fetch the latest EIA860 survey data------------------------------------------------------------------------------
-readEIA860sheet <- function(year,local_directory,
-                            xlsx_file="3_1_Generator") {
-  # Downoads EIA zip file, unzips it to local_directory,
+readEIA860sheet <- function(year,local_directory,xlsx_file){
+  # Downloads EIA zip file, unzips it to local_directory,
   #     then extracts the named sheet. Column labels are cleaned up
   #     into valid variable names
-  #lbry<-c("data.table", "RPostgreSQL",  "tidyr", "dplyr","arrow",
-         # "tools","lubridate", "Hmisc", "here", "readxl","stringr")
-  #test <- suppressMessages(lapply(lbry, require, character.only=TRUE, warn.conflicts = FALSE, quietly = TRUE))
-  #rm(test,lbry)
-  #  year = 2020
-  #  local_directory = here("raw_data")
-  EIA_path = "https://www.eia.gov/electricity/data/eia860/xls/"
-  zip_file_name = paste0("eia860",year,".zip")
   
-  url = paste0(EIA_path,zip_file_name) 
-  local_file = paste0(local_directory,zip_file_name)
-  cat(paste0(local_file,"\n"))
-  download.file(url,local_file,method="curl")
-  unzip(local_file,exdir=local_directory)
-  #  xlsx_file="3_1_Generator"
-  sheet="Operable"
+  #set the download path as a function of the latest year
+  download_path = paste0("https://www.eia.gov/electricity/data/eia860/xls/eia860",year,'.zip')
+  #set the local directory to download to as a function of the latest year
+  local_file = paste0(local_directory,"/eia860",year,".zip")
+  #download the .zip file from the EIA website (IDEALLY I'd like to be able to download just the file we use...)
+  download.file(download_path,local_file,method="curl")
+  #Unzip the specified file
+  unzip(local_file,file=paste0(xlsx_file,"_Y",year,".xlsx"),exdir=local_directory)
+  #get the full filepath for the file we just unzipped as a function of the current year
   file_name = paste0(local_directory,"/",xlsx_file,"_Y",year,".xlsx")
-  nms <- names(read_excel(file_name, sheet, n_max = 0,skip=1))
-  good_names = gsub("[?())]","",nms)
-  good_names = gsub("[[:punct:][:blank:]/]","_",good_names)
-  allStatesData = data.table(read_excel(file_name, sheet,skip=2,col_names=good_names))
-  va_gen_capacity = allStatesData[State=="VA",][,State:=NULL,]
-  va_gen_capacity[,RTO_ISO_Location_Designation:=RTO_ISO_Location_Designation_for_Reporting_Wholesale_Sales_Data_to_FERC]
-  va_gen_capacity[,id:=paste0(Plant_Code,"_",Generator_ID)]
+  #get a list of the column names
+  nms <- names(read_excel(file_name, sheet='Operable', n_max = 0,skip=1))
+  #format the column names
+  nms <- gsub("[?())]","",nms) %>% str_replace_all(" ","_") %>% str_replace("/","_")
+  #read in the full dataset with the new column names
+  gen_capacity_data <- data.table(read_excel(file_name, sheet='Operable',skip=2,col_names=nms))
+  gen_capacity_data <- gen_capacity_data %>% filter(State=="VA") %>% select(-State) %>%
+    rename(RTO_ISO_Location_Designation=RTO_ISO_Location_Designation_for_Reporting_Wholesale_Sales_Data_to_FERC) %>%
+    mutate(id=paste0(Plant_Code,"_",Generator_ID))
+  #clean up the download
+  unlink(file_name,recursive=TRUE)
+  unlink(local_file,recursive=TRUE)
   setkey(id)
-  return(va_gen_capacity)
+  return(gen_capacity_data)
 }
 
 #Retrieve EIA860 survey data for the latest year-----------------------------------------------------------------
@@ -37,19 +35,24 @@ readEIA860sheet <- function(year,local_directory,
 # Then filter for solar (PV) and wind (WT)  and storage (BA)
 #    Virginia doesn't have wind or storage up to 2020
 #
+#Set the directory to download to
 local_directory = here("raw_data")
+#Set the year we want to download
 # Bypass this test, if you are sure that new data is available
 if (month(now())<7) {
   year = year(now())-2
 } else {
   year = year(now())-1
 }
+#Set the file we want to unzip from the .zip download
+xlsx_file="3_1_Generator"
 # This function call returns all Virginia active generation units
-va_gen_plants = readEIA860sheet(year,local_directory,xlsx_file="3_1_Generator")
+va_gen_plants = readEIA860sheet(year,local_directory,xlsx_file)
+#Write the cleaned generation file to the database
 dbRemoveTable(db,"eia_plant_capacities")
 dbWriteTable(db,"eia_plant_capacities",va_gen_plants,append=F,row.names=F)
 
-# You can filter for solar, wind or storage
-va_solar = va_gen_plants[Prime_Mover=="PV"]
-va_solar[,id:=paste0(Plant_Code,"_",Generator_ID)]
-setkey(va_solar,id)
+# You can filter for solar, wind or storage (redundant, this is not saved here and is done again in calculations)
+#va_solar = va_gen_plants[Prime_Mover=="PV"]
+#va_solar[,id:=paste0(Plant_Code,"_",Generator_ID)]
+#setkey(va_solar,id)
