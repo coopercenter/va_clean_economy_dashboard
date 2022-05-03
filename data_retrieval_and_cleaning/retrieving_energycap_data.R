efdfe487d97f11a79e6baba2def1e9957f6daefb
Emily Lien <egl6a@virginia.edu>
2022-05-03 18:55:59 -0400

    moving calculation steps to the data cleaning stage

--- data_retrieval_and_cleaning/retrieving_energycap_data.R
+++ data_retrieval_and_cleaning/retrieving_energycap_data.R
@@ -90,7 +90,46 @@ energyCAP_data <- energyCAP_data %>% filter(commodity.commodityCode=='ELECTRIC')
 
 rm(energyCAP_building_data,energyCAP_meter_data,energyCAP_savings_data,columns_to_keep)
 
+#create a categorical size range column
+breaks <- c(5000.0,50000.0,100000.0,250000.0,500000.0,990000.0)
+tags <- c("5,000 - 50,000","50,001 - 100,000","100,001 - 250,000","250,001 - 500,000","500,001 - 990,000")
+energyCAP_data <- energyCAP_data %>% 
+  #create the size range column
+  mutate(size_range = cut(size.value,breaks=breaks, include.lowest=TRUE,right=FALSE,labels=tags))
+
+rm(breaks,tags)
+
+#replace NA with Information Missing
+energyCAP_data$primaryUse.primaryUseInfo <- energyCAP_data$primaryUse.primaryUseInfo %>% replace_na('Information Missing')
+
+#in parent.placeInfo, get a fresh column so that VCCS, VCU, and VDOT localities can fall under their parent umbrellas
+energyCAP_data$placeInfoSimple <- energyCAP_data$parent.placeInfo
+
+vccs <- unique(energyCAP_data$placeInfoSimple[grep("VCCS_",energyCAP_data$placeInfoSimple,perl=TRUE)])
+vcu <- unique(energyCAP_data$placeInfoSimple[grep("VCU_",energyCAP_data$placeInfoSimple,perl=TRUE)])
+vdot <- unique(energyCAP_data$placeInfoSimple[grep("VDOT_",energyCAP_data$placeInfoSimple,perl=TRUE)])
+
+#make a function replace the values
+simplify_place_information <- function(agency, replacement_name){
+  for (item in agency){
+    energyCAP_data$placeInfoSimple <- replace(energyCAP_data$placeInfoSimple,energyCAP_data$placeInfoSimple==item,replacement_name)
+  }
+  return(energyCAP_data$placeInfoSimple)
+}
+
+energyCAP_data$placeInfoSimple <- simplify_place_information(vccs,'Virginia Community Colleges (VCCS)')
+energyCAP_data$placeInfoSimple <- simplify_place_information(vcu,'Virginia Commonwealth University (VCU)')
+energyCAP_data$placeInfoSimple <- simplify_place_information(vdot,'Virginia Department of Transportation (VDOT)')
+
+rm(vccs,vcu,vdot)
+
+#replace AGENCY in placeInfoSimple with the corresponding placeInfo value
+agency_indices <- grep('AGENCY',energyCAP_data$placeInfoSimple,fixed=TRUE)
+for (item in agency_indices){
+  energyCAP_data$placeInfoSimple[item] <- energyCAP_data$placeInfo[item]
+}
+
 
 #write to the database
 dbRemoveTable(db,"energycap_place_meter_and_savings_data")
-dbWriteTable(db,"energycap_place_meter_and_savings_data",energyCAP_data)
+dbWriteTable(db,"energycap_place_meter_and_savings_data",energyCAP_data,row.names=FALSE)
